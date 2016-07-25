b361e0b93309d09b39abac81416a002612b522a2
Sergey Zelenov <s.zelenov@btc-solutions.net>
2016-07-25 12:36:30 +0300

    Change in code style: remove <p> tag from javaDoc

--- flash.sh
+++ flash.sh
@@ -17,50 +17,50 @@ done
 
 
 echo -e "starting process at `date_formatted`"
-# adb kill-server
-# adb devices
-# adb wait-for-device
-# adb reboot bootloader
+adb kill-server
+adb devices
+adb wait-for-device
+adb reboot bootloader
 
 echo -e "\nstart flash aboot at `date_formatted`"
-# fastboot flash aboot emmc_appsboot.mbn
+fastboot flash aboot emmc_appsboot.mbn
 echo -e "end flash aboot at `date_formatted` \n"
 
 echo -e "start flash boot at `date_formatted`"
-# fastboot flash boot boot.img
+fastboot flash boot boot.img
 echo -e "end boot at `date_formatted` \n"
 
 echo -e "start flash system at `date_formatted`"
-# fastboot flash system system.img
+fastboot flash system system.img
 echo -e "end flash system at `date_formatted` \n"
 
 if [ $USERDATA -eq 1 ]
 	then
 		echo -e "start flash userdata at `date_formatted`"
-		# fastboot flash userdata userdata.img
+		fastboot flash userdata userdata.img
 		echo -e "end flash userdata at `date_formatted` \n"
 fi
 
 echo -e "start flash recovery at `date_formatted`"
-# fastboot flash recovery recovery.img
+fastboot flash recovery recovery.img
 echo -e "end flash recovery at `date_formatted` \n"
 
 echo -e "start flash cache at `date_formatted`"
-# fastboot flash cache cache.img
+fastboot flash cache cache.img
 echo -e "end flash cache at `date_formatted`\n"
  
  
 if [ $RADIO -eq 1 ]
 	then
 		echo -e "start flash radio at `date_formatted`"
-		# fastboot flash modem radio\NON-HLOS.bin
-		# fastboot flash sbl1 radio\sbl1.mbn
-		# fastboot flash rpm radio\rpm.mbn
-		# fastboot flash tz radio\tz.mbn
+		fastboot flash modem radio\NON-HLOS.bin
+		fastboot flash sbl1 radio\sbl1.mbn
+		fastboot flash rpm radio\rpm.mbn
+		fastboot flash tz radio\tz.mbn
 		echo -e "end flash radio at `date_formatted` \n"
 fi
 
-# fastboot reboot      
+fastboot reboot      
 echo -e "end process at `date_formatted` \n"
 
 
